0cd9894e2358eb257643265ee1a2ff35e1dd718f
sudaisahmad01 <sudaisahmad1107@gmail.com>
2026-08-21 16:12:21 +0500

    Add Python calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+#sub se pehlay user 2 numbers enter karega phir sub operations perform honge
+#user 1st number enter karay ga or int us ko integer ma convert karay ga
+a = int(input("Enter an integer  number: "))
+print(a)
+#user 2nd number enter karay ga or float us ko float (decimal number) ma convert karay ga
+b = float(input("Enter a float  number: "))
+print(b)
+#ye code dono numbers ko add ker ke  result ma display karay ga 
+print(a+b)
+#ye code dono numbers ko subtract ker ke   result ma display karay ga
+print(a-b)
+# ye code dono numbers ko multiply ker  ke result ma display karay ga
+print(a*b)
+#ye code dono numbers ko divide ker  ke result ma display karay ga
+print(a/b)
+#ye code dono numbers ka modulus ker ke result ma display karay ga
+print(a%b)
+#ye code dono numbers ki floor division ker  result ma display karay ga
+print(a//b)
+#ye code dono numbers ka exponentation le ker  result ma display karay ga
+print(a**b)
